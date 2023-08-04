4b5d8822a069edf670b4812e75c8cac4d43f748a
sjklan <dpeltms4@gmail.com>
2023-08-04 12:39:02 +0900

    appvue

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -1,5 +1,59 @@
 <template>
   <div>
+    ID :<input type="text" v-model="user.id">
+    Password: <input type="password" v-model="user.password">
+    <button @click="login()">login</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import router from './router'; 
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      token: '',
+      user: {
+        id: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      axios.get(`http://localhost:8000/api/token?id=${this.user.id}&password=${this.user.password}`)
+      .then(res => {
+        console.log(res.data);
+        if (res.data.errflg === 0) {
+          this.token = res.data.token; 
+          router.push('/listpage');
+        } 
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!-- <template>
+  <div>
+    ID :<input type="text"> 
+    비밀번호 : <input type="text">
     <button @click="login()">로그인</button>
   </div>
 </template>
@@ -16,7 +70,7 @@ export default {
   },
   methods: {
     login() {
-      axios.get('http://localhost:8000/api/token?id=gogolet')
+      axios.get('http://localhost:8000/api/token?id=')
       .then(res=> {
         console.log(res.data);
         this.token = res.data.token;
@@ -35,4 +89,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
